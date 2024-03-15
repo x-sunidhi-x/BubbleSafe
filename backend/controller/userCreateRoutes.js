6108dc70ed0f29513ba9c58fd8e0349831c2c197
x-sunidhi-x <gopalansunidhi@gmail.com>
2024-03-16 05:26:57 +0530

    updated

--- backend/controller/userCreateRoutes.js
+++ backend/controller/userCreateRoutes.js
@@ -1,59 +1,117 @@
+//userCreateRoutes.js
 const express = require("express")
-const userCreateRoutes=express.Router()
+const bcrypt = require("bcrypt")
+const userCreateRoutes = express.Router()
 const UserSchema = require("../model/UserSchema")
+const { createUserToken } = require("../auth/auth")
+// userCreateRoutes.get("/", (req, res) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*")
+//   res.setHeader("Access-Control-Allow-Credentials", "true");
+//   res.setHeader("Access-Control-Max-Age", "1800");
+//   res.setHeader("Access-Control-Allow-Headers", "content-type");
+//   res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
+//    });
+
+const { getOTP, verifyOTP } = require("../auth/auth")
+
+// SEND-FORGOT-PASSWORD-OTP
+userCreateRoutes.post("/send-otp", (req, res) => {
+  const { email } = req.body    
+  getOTP(email)
+  res.json({ message: "Success" })
+  
+})
 
 userCreateRoutes.get("/", (req, res) => {
-    UserSchema.find()
-      .then(data => {
-        res.json(data);
-      })
-      .catch(err => {
-        res.status(500).json({ error: err.message });
-      });
-  });
+  UserSchema.find()
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      res.status(500).json({ error: err.message });
+    });
+});
 
-  userCreateRoutes.post("/signup", (req, res) => {
-    const { name, email, password} = req.body
-    UserSchema.create(
-      {
-        name: name,
-        email: email,
-        password: password,
-      })
-      .then(data=>{
-          res.json({ message: "Success" })
-      })
-      .catch(err=>{
-        res.json({ error: err, status: 500 })
-      })
-  });
-  
-  userCreateRoutes.post("/login", (req, res) => {
-    const { email, password } = req.body
-    UserSchema.findOne({ email: email }, (err, user) => {
-      //check if user exists
-      if (err) {
-        res.json({ error: "Unable to fetch user", status: 500 })
+//SIGNUP
+
+
+userCreateRoutes.post("/signup", (req, res) => {
+  const { name, email, password, otp } = req.body
+  const match= verifyOTP(email,otp)
+  if(match){
+  UserSchema.create(
+    {
+      name: name,
+      email: email,
+      password: password,
+    })
+    .then(data=>{
+      const accessToken = createUserToken({ email: email })
+        res.cookie("user", accessToken, {
+          sameSite: "none",
+          secure: true,
+          httpOnly: true,
+          maxAge: 60 * 60 * 24 * 1000,
+        })
+        res.json({ message: "Success" })
+    })
+    .catch(err=>{
+      res.json({ error: err, status: 500 })
+    })
+  }
+    
+});
+
+//LOGIN
+userCreateRoutes.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  UserSchema.findOne({ email: email })
+    .then(user => {
+      if (!user) {
+        res.json({ error: "User does not exist", status: 500 });
       } else {
-        if (!user) {
-          res.json({ error: "User does not exist", status: 500 })
-        } else {
-          //compare email and password with stored values
-          UserSchema.findOne({email:email,password:password},(err,user)=>{
-            if (err) {
-              res.json({ error: "Unable to fetch user", status: 500 })
+        const hashedPassword = user.password;
+        bcrypt.compare(password, hashedPassword)
+          .then((match) => {
+            if (match) {
+              const accessToken = createUserToken({ email: email });
+              res.cookie("user", accessToken, {
+                sameSite: "none",
+                secure: true,
+                httpOnly: true,
+                maxAge: 60 * 60 * 24 * 1000,
+              });
+              res.json({ message: "Success" });
             } else {
-            if(!user){
-              res.json({error:"Invalid login details", status:500})
+              res.json({ error: "Invalid credentials", status: 500 });
             }
-            else{
-              res.json({message:"success"});
-            }
-          }
           })
-        }
+          .catch((err) => {
+            console.log(err);
+            res.json({ error: "Failed", status: 500 });
+          });
       }
     })
-  })
-  
-  module.exports = userCreateRoutes
+    .catch(err => {
+      console.log(err);
+      res.json({ error: "Unable to fetch user", status: 500 });
+    });
+});
+
+
+//LOGOUT
+userCreateRoutes.get("/logout", (req, res) => {
+  res.cookie(
+    "user",
+    {},
+    {
+      sameSite: "none",
+      secure: true,
+      httpOnly: true,
+      maxAge: 60 * 60 * 24 * 1000,
+    }
+  )
+  res.json({ message: "Success" })
+})
+
+module.exports = userCreateRoutes
--- frontend/src/Components/StrangerDanger.js
+++ frontend/src/Components/StrangerDanger.js
@@ -1,20 +1,21 @@
 import './StrangerDanger.css'
-import child from './both.png'
+import child from './images/both.png'
 import React, { useState, useEffect, useRef, Component } from 'react';
 import useSound from 'use-sound'
 import Confetti from './Confetti'
-import buzzer from './buzzerwav-14908.mp3'
-import correctsound from './mixkit-correct-answer-tone-2870.wav'
+import buzzer from './images/invalid-selection-39351.mp3'
+import correctsound from './images/mixkit-correct-answer-tone-2870.wav'
 import Axios from 'axios';
-// import stranger from './stranger.png'
+import AudioComp from './AudioComp';
 function StrangerDanger(){
     const [isCorrect, setIsCorrect] = useState(false);
     const [isWrong, setIsWrong] = useState(false);
     let wonAudio = new Audio(correctsound);
     let lostAudio = new Audio(buzzer);
-    
+ 
     return(
-        <center>
+        <center className='main-stranger'>   
+          <AudioComp/>   
         <h1>STRANGER DANGER</h1>
         <p>Hey look! It's a man who lives in your neighbourhood.
 He just moved in last week.</p>
@@ -33,13 +34,13 @@ He just moved in last week.</p>
         <h2>What should you say?</h2>
 
         <button 
-        className={`Btn ${isWrong === true && 'wrong'}`}
+        className={`Butn ${isWrong === true && 'wrong'}`}
         onClick={() => { setIsWrong(!isWrong); setIsCorrect(false);if(!isWrong) lostAudio.play() }}
       >
         Yes, I will come to your house. I love sweets!
       </button>
         <button 
-        className={`Btn ${isCorrect === true && 'correct'}`}
+        className={`Butn ${isCorrect === true && 'correct'}`}
         onClick={() => {
           setIsCorrect(!isCorrect);
           if (!isCorrect) wonAudio.play();
